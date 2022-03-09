b2b3cd70a65c8508fbd5509cf1e0b99334649051
nijatmammadovv <tu7q78xlv@code.edu.az>
2022-03-09 11:00:37 +0400

    Add project files.

---
+++ NumLength Method Task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumLength_Method_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n The number {0} contains number of digits : {1} ", num, NumLength(num, 0));
+            Console.ReadLine();
+        }
+
+        static int NumLength(int n1, int numberless)
+        {
+            if (n1 == 0)
+                return numberless;
+
+            return NumLength(n1 /= 10, ++numberless);
+
+        }
+    }
+}
